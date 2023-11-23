95342022dd7b811a0e611e635ff3cf828ffa87e9
Koleduke <ydel2014@gmail.com>
2023-11-23 11:31:43 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        char c = character;
+
+        if (!(c >= 'a' && c <= 'z') && !(c >= 'A' && c <= 'Z'))
+        {System.out.println("wrong alphabet!");}
+        else{ if(isVowel(c)){
+            System.out.println("Vowel");}
+            else{System.out.println("Consonant");}
+        }
+
+    }
+    public static boolean isVowel(char c) {
+        return "AEIOUaeiou".indexOf(c) != -1;
     }
 }
